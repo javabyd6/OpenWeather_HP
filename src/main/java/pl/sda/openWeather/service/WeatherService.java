23d9070fdbf4aa7ca65b5ebf644c361278da4f3a
Hubert Perczyński <hubertperczynski@gmail.com>
2019-03-10 15:17:44 +0100

    Elo URLo

--- src/main/java/pl/sda/openWeather/service/WeatherService.java
+++ src/main/java/pl/sda/openWeather/service/WeatherService.java
@@ -2,7 +2,6 @@ package pl.sda.openWeather.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import pl.sda.openWeather.model.Weather;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,7 +13,7 @@ public class WeatherService {
     public static Weather getCityWeather(String city) throws MalformedURLException
     {
         ObjectMapper mapper = new ObjectMapper();
-        String apiKey = "key=3e11d5ea24914379b62120104191003";
+        String apiKey = "key=c478f8634402452397a122330191003";
         URL finalURL = new URL("http://api.apixu.com/v1/current.json?"+apiKey+"&q="+city);
         Weather pogoda = null;
         try
